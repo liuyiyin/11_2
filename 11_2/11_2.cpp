b92ee5d5ee910aa12e6096835015dc4bb5b602df
lenovo <lenovo@LAPTOP-P8K9KTT5>
2020-05-15 14:57:21 +0800

    11_2

--- 11_2/11_2.cpp
+++ 11_2/11_2.cpp
@@ -2,10 +2,23 @@
 //
 
 #include "stdafx.h"
-
+#include <opencv2/opencv.hpp>
+using namespace cv;
 
 int main()
 {
+	cv::Mat src = imread("D:\\etest.jpg");
+	cv::Mat dst;
+	cv::Mat a[3];
+	cv::Mat b[3];
+	split(src, a);
+	for (int i = 0; i < 3; i++) {
+		equalizeHist(a[i], b[i]);
+	}
+	merge(b, 3, dst);
+	cv::imshow("src", src);
+	cv::imshow("dst", dst);
+	cv::waitKey(0);
     return 0;
 }
 
